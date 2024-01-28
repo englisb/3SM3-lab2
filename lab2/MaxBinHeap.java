1193741e4892d16acaa698b2eb1cfbe664ecee81
englisb <englisb@mcmaster.ca>
2024-01-28 14:24:45 -0500

    skeleton

---
+++ lab2/MaxBinHeap.java
@@ -0,0 +1,38 @@
+package lab2;
+
+public class MaxBinHeap {
+
+    private int keys[], size;
+
+    public MaxBinHeap(int n){
+
+    }
+
+    public MaxBinHeap(int[] a){
+
+    }
+    
+    public int getSize(){
+        return 0;
+    }
+
+    public void insert(int x){
+
+    }
+
+    public int readMax() throws RuntimeException{
+        return 0;
+    }
+
+    public int deleteMax() throws RuntimeException{
+        return 0;
+    }
+
+    public String toString(){
+        return "";
+    }
+
+    public static void sortArray(int k, int[] a){
+        
+    }
+}
---
+++ lab2/YoungT.java
@@ -0,0 +1,48 @@
+package lab2;
+
+public class YoungT {
+
+    private int tab[][], finInts;
+
+    private static final int infinity = 0;
+    
+    public YoungT(int k, int n, int infinity){
+
+    }
+
+    public YoungT(int[][] a){
+
+    }
+
+    public int getNumElem(){
+        return 0;
+    }
+
+    public int getInfinity(){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public boolean isFull(){
+        return false;
+    }
+
+    public int readMin() throws RuntimeException{
+        return 0;
+    }
+
+    public int deleteMin() throws RuntimeException{
+        return 0;
+    }
+
+    public boolean find(int x) throws RuntimeException{
+        return false;
+    }
+
+    public String toString(){
+        return "";
+    }
+}
