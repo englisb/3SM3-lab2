952d16be9f0d04258c174648afeb3684d96ce670
englisb <englisb@mcmaster.ca>
2024-02-04 16:38:39 -0500

    damn

--- lab2/MaxBinHeap.java
+++ lab2/MaxBinHeap.java
@@ -2,32 +2,26 @@ package lab2;
 
 public class MaxBinHeap {
 
-    private int keys[], size;
+    private int keys[];
+    private int size = 0;
 
     public MaxBinHeap(int n){
         if(n < 10){                 //initialize keys[] to have a default size 10 (+ 1 for empty first elements)
-            size = 10;
             keys = new int[11];
         }
         else{
             keys = new int[n + 1];
-            size = n;
         }
-        
-        for (int i = 0; i < keys.length; i++)
-            keys[i] = -1;
     }
 
     public MaxBinHeap(int[] a){
-        if (a.length == 0) {
-            size = 10;
-            keys = new int[size + 1];
-        }
+        if (a.length == 0) 
+            keys = new int[11];
+        
         else{
-            size = a.length;
-            keys = new int[size + 1];
-            for (int i : a) {
-                insert(i);
+            keys = new int[a.length + 1];
+            for (int k : a) {
+                insert(k);
             }
         }
     }
@@ -39,50 +33,46 @@ public class MaxBinHeap {
 
     public void insert(int x){
         int xIndex = insertAtEnd(x);
+        
+        percolateUp(xIndex);
+        
+        size++;
+    }
+    
+    private int percolateUp(int index){
         int tempKey;
-
         //fix the tree by percolating up
-        while(keys[xIndex] > keys[xIndex / 2]){         //edge case for when x is percolated to the root is covered b/c keys[0] == -1
-            tempKey = keys[xIndex / 2];                 //swapping
-            keys[xIndex / 2] = keys[xIndex];
-            keys[xIndex] = tempKey;
-            xIndex = xIndex / 2;
+        while(keys[index / 2] != 0 && keys[index] > keys[index / 2]){         //percolating up the key just added in case it breaks the max heap property
+            tempKey = keys[index / 2];                 //swapping
+            keys[index / 2] = keys[index];
+            keys[index] = tempKey;
+            index = index / 2;
         }
 
-        size++;
+        return index;                                   //return the new index of the node percolated node
     }
 
     private int insertAtEnd(int x){
-        int i, j;
+        int j;
 
-        for (i = keys.length - 1; i > 0; i--) {                 //traversing the keys backwards until a non empty (non -1) element is found
-            if(keys[i] != -1){
-                if (i == keys.length - 1) {            //if the first non empty key is the final element in the array of keys, need to make a new array of keys double in length and copy elements over
-                    int[] tempKeys = new int[keys.length * 2];      //allocated the temporary keys array of double size
-                    tempKeys[0] = -1;                               //set the offset index as -1
-                    
-                    for (j = 1; j < keys.length; j++)               //copy all keys from the original array until the end
-                        tempKeys[j] = keys[j];
+        
+        if (size == keys.length - 1) {            //if the first non empty key is the final element in the array of keys, need to make a new array of keys double in length and copy elements over
+            int[] tempKeys = new int[keys.length * 2];      //allocated the temporary keys array of double size
+            
+            for (j = 1; j <= size; j++)               //copy all keys from the original array until the end
+                tempKeys[j] = keys[j];
 
-                    tempKeys[j] = x;                                //will be keys.length + 1 at this point, now insert x as the final key in tempKeys 
-
-                    for (j++; j < tempKeys.length ; j++)            //populate the remaining elements in tempKeys as empty (-1)
-                        tempKeys[j] = -1;
-                    
-                    keys = tempKeys;
-                    return j;
-                }
-
-                keys[i + 1] = x;                        //otherwise, insert x at the first empty element
-                break;
-            }
+            tempKeys[j] = x;                                //j will be size + 1 at this point, now insert x as the final key in tempKeys 
+            keys = tempKeys;
+            return j;
         }
 
-        return i;
+        keys[size + 1] = x;                    //insert x at the first empty element
+        return size + 1;                       //return where the insertion was performed
     }
 
     public int readMax() throws RuntimeException{
-        if(keys[1] == -1){
+        if(size == 0){
             RuntimeException ex = new RuntimeException("Cannot return max element, heap is empty!");
             throw ex;
         }
@@ -90,39 +80,50 @@ public class MaxBinHeap {
     }
 
     public int deleteMax() throws RuntimeException{
-        int i, minKey, currIndex = 1, tempKey = 0, maxIndex = 1;
+        int maxKey = keys[1], origSize = size;
 
-        for (i = keys.length - 1; i == -1; i--){
-
+        if (maxKey == 0) {
+            RuntimeException ex = new RuntimeException("Cannot delete max element, heap is empty!");
+            throw ex;
         }
 
-        minKey = keys[i + 1];
-        keys[maxIndex] = minKey;
+        heapify(keys[size]);
 
-        while(currIndex * 2 != -1 && currIndex * 2 + 1 != -1){
-            if(minKey > keys[currIndex * 2] || minKey > keys[currIndex * 2 + 1])
+        for (int i = size; i >= origSize; i--)          //for delete max its a more convenient representation to have empty keys be 0
+            keys[i] = 0;                                //but size handles the the proper end point anyways
+
+        return maxKey;
+    }
+
+    private void heapify(int keyOfIntrst){
+        int currIndex = 1, tempKey;
+        keys[currIndex] = keyOfIntrst;
+
+        while(currIndex * 2 + 1 <= size){
+            if(keyOfIntrst > keys[currIndex * 2] && keyOfIntrst > keys[currIndex * 2 + 1])
                 break;
             currIndex = keys[currIndex * 2] > keys[currIndex * 2 + 1] ? currIndex * 2 : currIndex * 2 + 1;
             tempKey = keys[currIndex];
             keys[currIndex / 2] = tempKey;
-            keys[currIndex] = minKey;
+            keys[currIndex] = keyOfIntrst;
         }
 
-
-        return 0;
+        size--;
     }
 
     public String toString(){
         String keysString = "";
 
         //iterate through each key in the list
-        for (int k = 1; keys[k] != -1 && k < keys.length; k++)
+        for (int k = 1; k <= size && keys[k] != 0; k++)
             keysString += keys[k] + ", ";
 
         return keysString;
     }
 
-    public static void sortArray(int k, int[] a){
-        
+    public static void sortArray(int[] a){       //(int k, int[] a)???
+        MaxBinHeap tempHeap = new MaxBinHeap(a);
+
+        tempHeap.heapify(tempHeap.keys[tempHeap.getSize()]);
     }
 }
