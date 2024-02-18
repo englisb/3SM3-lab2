5a60d1d67b21fc802832282f55d308a410bcb81a
englisb <englisb@mcmaster.ca>
2024-02-17 21:39:54 -0500

    Update MaxBinHeap.java

--- lab2/MaxBinHeap.java
+++ lab2/MaxBinHeap.java
@@ -7,25 +7,29 @@ public class MaxBinHeap {
 
     public MaxBinHeap(int n){
         if(n < 10){                 //initialize keys[] to have a default size 10 (+ 1 for empty first elements)
-            keys = new int[11];
+            keys = new int[10];
         }
         else{
-            keys = new int[n + 1];
+            keys = new int[n];
         }
     }
 
     public MaxBinHeap(int[] a){
         if (a.length == 0) 
-            keys = new int[11];
+            keys = new int[10];
         
         else{
-            keys = new int[a.length + 1];
-            for (int k : a) {
-                insert(k);
-            }
+            keys = buildHeap(a);
         }
     }
     
+    private int[] buildHeap(int[] a){
+        for (int i = a.length - 1; i > 0; i--) {
+            heapifySingleElem(a, i);
+        }
+        
+        return a;
+    }
 
     public int getSize(){
         return size;
@@ -56,7 +60,7 @@ public class MaxBinHeap {
         int j;
 
         
-        if (size == keys.length - 1) {            //if the first non empty key is the final element in the array of keys, need to make a new array of keys double in length and copy elements over
+        if (size == keys.length) {            //if the first non empty key is the final element in the array of keys, need to make a new array of keys double in length and copy elements over
             int[] tempKeys = new int[keys.length * 2];      //allocated the temporary keys array of double size
             
             for (j = 1; j <= size; j++)               //copy all keys from the original array until the end
@@ -76,18 +80,18 @@ public class MaxBinHeap {
             RuntimeException ex = new RuntimeException("Cannot return max element, heap is empty!");
             throw ex;
         }
-        return keys[1];
+        return keys[0];
     }
 
     public int deleteMax() throws RuntimeException{
-        int maxKey = keys[1], origSize = size;
+        int maxKey = keys[0], origSize = size;
 
         if (maxKey == 0) {
             RuntimeException ex = new RuntimeException("Cannot delete max element, heap is empty!");
             throw ex;
         }
 
-        heapify(keys, size);
+        heapifySingleElem(keys, size - 1);
 
         size--;
 
@@ -97,17 +101,17 @@ public class MaxBinHeap {
         return maxKey;
     }
 
-    private void heapify(int[] keysArr, int index){
-        int currIndex = 1, tempKey, keyOfIntrst = keysArr[index];
-        keysArr[currIndex] = keyOfIntrst;
+    private void heapifySingleElem(int[] keysArr, int index){
+        int tempKey, keyOfIntrst = keysArr[index];
+        keysArr[index] = keyOfIntrst;
 
-        while(currIndex * 2 + 1 <= size){
-            if(keyOfIntrst > keys[currIndex * 2] && keyOfIntrst > keys[currIndex * 2 + 1])
+        while(index * 2 + 1 <= size){
+            if(keyOfIntrst > keys[index * 2] && keyOfIntrst > keys[index * 2 + 1])
                 break;
-            currIndex = keys[currIndex * 2] > keys[currIndex * 2 + 1] ? currIndex * 2 : currIndex * 2 + 1;
-            tempKey = keys[currIndex];
-            keys[currIndex / 2] = tempKey;
-            keys[currIndex] = keyOfIntrst;
+            index = keys[index * 2] > keys[index * 2 + 1] ? index * 2 : index * 2 + 1;
+            tempKey = keys[index];
+            keys[index / 2] = tempKey;
+            keys[index] = keyOfIntrst;
         }
     }
 
@@ -115,7 +119,7 @@ public class MaxBinHeap {
         String keysString = "";
 
         //iterate through each key in the list
-        for (int k = 1; k <= size && keys[k] != 0; k++)
+        for (int k = 0; k <= size && keys[k] != 0; k++)
             keysString += keys[k] + ", ";
 
         return keysString;
@@ -124,6 +128,6 @@ public class MaxBinHeap {
     public static void sortArray(int[] a){       //(int k, int[] a)???
         MaxBinHeap tempHeap = new MaxBinHeap(a);
 
-        tempHeap.heapify(a, a.length - 1);
+        tempHeap.heapifySingleElem(a, a.length - 1);
     }
 }
